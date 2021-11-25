6534a88aa1edf12708b0299886a930fb2415b4c9
HouzzyP <choclo99@gmail.com>
2021-11-25 20:50:03 -0300

    First Upload

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
+require('dotenv').config()
+require('./mongo.js')
+
+const Sentry = require('@sentry/node')
+const Tracing = require('@sentry/tracing')
+const Note = require('./models/Note')
 const express = require('express')
 const app = express()
-const logger = require('./loggerMiddleware')
 const cors = require('cors')
+const notFound = require('./middleware/notFound')
+const handleErrors = require('./middleware/handleErrors')
+const { Mongoose } = require('mongoose')
+
+
 
 
 
 app.use(express.json())
-
 app.use(cors())
-app.use(logger)
+app.use('/images', express.static('images'))
 
-let notes = [
-	{
-		id: 1,
-		content: 'HTML is easy',
-		date: '2019-05-30T17:30:31.098Z',
-		important: true
-	},
-	{
-		id: 2,
-		content: 'Browser can execute only Javascript',
-		date: '2019-05-30T18:39:34.091Z',
-		important: false
-	},
-	{
-		id: 3,
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		date: '2019-05-30T19:20:14.298Z',
-		important: true
-	}
-]
+Sentry.init({
+	dsn: 'https://c95c73f3b685490aa0ecb9f540f6cf1e@o1078093.ingest.sentry.io/6081624',
+	integrations: [
+		//enable HTTP calls tracing
+	  	new Sentry.Integrations.Http({ tracing: true }),
+	  	//enable Express.js middleware tracing
+	  	new Tracing.Integrations.Express({ app }),
+	],
+  
+	// Set tracesSampleRate to 1.0 to capture 100%
+	// of transactions for performance monitoring.
+	// We recommend adjusting this value in production
+	tracesSampleRate: 1.0,
+})
+
 //   const app = http.createServer((request, response) => {
 //     response.writeHead(200, { 'Content-Type': 'application/json' })
 //     response.end(JSON.stringify(notes))
@@ -39,61 +43,88 @@ app.get('/', (request,response) =>{
 	response.send('<h1>hello world</h1>')
 })
 
+
 app.get('/api/notes',(request,response) =>{
-	response.json(notes)
+	Note.find({}).then(notes =>{
+		response.json(notes)	
+	})
+	
 })
 
-app.get('/api/notes/:id',(request,response) =>{
-	const id = Number(request.params.id)
-	const note = notes.find(note => note.id === id)
-	if(note){
-		response.json(note)
-	}else{
+app.get('/api/notes/:id',(request,response,next) =>{
+	const {id} = request.params
+
+	Note.findById(id).then(note =>{
+		if(note) return response.json(note)
 		response.status(404).end()
-	}
+		
+	}).catch(err =>{
+		next(err)
+	})
+	
 })
 
-app.delete('/api/notes/:id', (request,response)=>{
-	const id = Number(request.params.id)
-	notes = notes.filter(note => note.id !== id)
-	response.status(204).end()
-})
-
-app.post('/api/notes',(request,response) =>{
+app.put('/api/notes/:id', (request,response,next)=>{
+	const id = request.params.id
 	const note = request.body
 
-	if(!note || !note.content){
+
+	const newNoteInfo = {
+		content: note.content,
+		important: note.important
+	}
+	
+	Note.findByIdAndUpdate(id, newNoteInfo, {new:true})
+		.then(result =>{
+			response.json(result)
+		})
+		.catch(err => next(err))
+
+})
+
+app.delete('/api/notes/:id', (request,response,next)=>{
+	const id = request.params.id
+	
+	Note.findByIdAndDelete(id)
+		.then(() =>response.status(204).end())
+		.catch(error => next(error))
+
+})
+
+app.post('/api/notes',(request,response, next) =>{
+	const note = request.body
+
+	if(!note.content){
 		return response.status(400).json({
 			error:'note.content is missing'
 		})
 	}
-	const ids = notes.map(note => note.id)
-	const maxId = Math.max(...ids)
 
-	const newNote = {
-		id: maxId + 1,
-		content:note.content,
-		important:  typeof note.important !== undefined ? note.important : false,
-		date: new Date().toISOString()
-
-	}
-	notes = [...notes, newNote]
-
-	response.status(201).json(newNote)
-})
-
-
-app.use((request,response) =>{
-	response.status(404).json({
-		error : 'Not Found'
+	const newNote = new Note({
+		content: note.content,
+		date: new Date(),
+		important: note.important || false
 	})
+
+	newNote.save().then( savedNote =>{
+		response.json(savedNote)
+	})
+		.catch(err => next(err))
+
+
 })
 
+//Middlewares
+app.use(notFound)
 
+app.use(Sentry.Handlers.errorHandler())
 
-const PORT = process.env.PORT || 3001
+app.use(handleErrors)
+
+const PORT = process.env.PORT
 
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
+
---
+++ mongo.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const connectionString = process.env.MONGO_DB_URI
+
+
+//conexion a mongodb
+
+mongoose.connect(connectionString )
+	.then(() =>{
+		console.log('Database connected')
+	}).catch( err => {
+		console.log(err)
+	})
+
+
+process.on('uncaughtException', () =>{
+	mongoose.connection.disconnect()
+})
+	
+
